750c021801ab998e8f009d965aa4ceee003b56d7
brayner barragan nieto <brayner.barragann@gmail.com>
2020-11-21 16:54:12 -0500

    Se implementa la atualizacin de los datos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 db=sqlite3.connect('data.db', check_same_thread=False)
 
@@ -26,7 +26,51 @@ def usuarios():
 
 @app.route('/usuarios/crear', methods=('GET', 'POST'))
 def crear_usuarios():
+    if request.method == 'GET':
+        return render_template('usuarios/crear.html')
 
-    return render_template('usuarios/crear.html')
+    nombres = request.form.get('nombres')    
+    apellidos = request.form.get('apellidos')    
+    email = request.form.get('email')    
+    password = request.form.get('password')
+
+    cursor=db.cursor()
+    cursor.execute(""" insert into usuarios(
+        nombres,
+        apellidos,
+        email,
+        password
+    )values(?,?,?,?)
+    """,(nombres, apellidos, email, password)) 
+    db.commit()
+    return redirect(url_for('usuarios'))
+
+@app.route('/usuarios/editar/<id>', methods=('GET', 'POST'))
+def editar(id):
+    act=db.execute('select * from usuarios where id=?',(id,)).fetchone()
+    if request.method == 'GET':
+        return render_template('usuarios/actualizar.html',fila=act)
+
+@app.route('/usuarios/<id>', methods=('GET', 'POST'))
+def guardar_cambios(id):
+
+    nombres = request.form.get('nombres')    
+    apellidos = request.form.get('apellidos')    
+    email = request.form.get('email')    
+    password = request.form.get('password')
+
+    cursor=db.cursor()
+    cursor.execute(' UPDATE usuarios SET nombres = ?, apellidos = ?, email = ?,password = ? WHERE id = ?',(nombres, apellidos, email, password, id)) 
+    db.commit()
+
+    return redirect(url_for('usuarios'))
+    
+    
+    
+
+
+    
+ 
+
 
 app.run(debug=True)    
